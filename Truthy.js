72d3ef043de409877475c9a453a10fe9598abc3c
https://github.com/Mathpatibaswaraj1993/Bootstrap.git <Mathpatibaswaraj1993>
2025-04-21 13:28:03 +0530

    changes

--- Truthy.js
+++ Truthy.js
@@ -28,6 +28,9 @@ if(Object.keys(emptyObj).length === 0 ){
 //Nullish  coalescing operator (??): null undefined
 
 let val1;
-val1 = 5 ?? 10
-
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
 console.log(val1)
+
